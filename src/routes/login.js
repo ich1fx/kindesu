dc41e4b3268655542d0dd3055df83dc4078cbeb3
iciepex <120244424+ich1fx@users.noreply.github.com>
2023-06-09 04:50:55 +0700

    .

--- src/routes/login.js
+++ src/routes/login.js
@@ -9,7 +9,7 @@ export const path = `/${Deno.env.get('SECRET')}`;
 export async function execute({ request, branch }) {
   const cookie = new CookieMap(request, { secure: true });
   await cookie.set('allowed', 'true');
-  ctx.response.redirect('/');
+ 
   return new Response("Redirecting",
     status: Status.Found,
     headers: mergeHeaders({ Location: request.url.origin }, cookie)
