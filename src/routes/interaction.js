f8114e5f90ed5eb8837947b6caffd3a53848fc2c
iciepex <120244424+ich1fx@users.noreply.github.com>
2023-06-09 04:46:46 +0700

    .

--- src/routes/interaction.js
+++ src/routes/interaction.js
@@ -3,18 +3,16 @@ import {
 } from 'tweetnacl';
 import {
   Status
-} from 'oak/mod.ts';
+} from 'http/server.ts';
 import * as events from '../events/mod.js';
 
 export const path = "/interaction";
 export const method = "POST";
   
-export async function execute(ctx) {
-  const branch = ctx.request.url.host.includes('--') ? ctx.request.url.host.split('--')[1].split('.deno.dev')[0].toUpperCase() : 'PROD';
-    
-  const body = await ctx.request.body({ type: 'text' }).value;
-  const timestamp = ctx.request.headers.get('x-signature-timestamp');
-  const signature = ctx.request.headers.get('x-signature-ed25519');
+export async function execute({ request, branch }) {
+  const body = await request.text();
+  const timestamp = request.headers.get('x-signature-timestamp');
+  const signature = request.headers.get('x-signature-ed25519');
     
   const valid = await sign.detached.verify(
     new TextEncoder().encode(timestamp + body),
@@ -22,12 +20,13 @@ export async function execute(ctx) {
     hexEncode(Deno.env.get(`${branch}_PUBLIC_KEY`))
   );
   if (!valid) {
-    ctx.response.body = 'Invalid Request!';
-    ctx.response.status = Status.Unauthorized;
+    return new Response('Invalid Request!', {
+      status: Status.Unauthorized
+    });
   } else {
     const interaction = JSON.parse(body);
     const event = Object.values(events).find(evt => evt.type === interaction.type);
-    if (event) return await event.execute({ ctx, branch, interaction });
+    if (event) return await event.execute({ request, branch, interaction });
   }
 };
 
