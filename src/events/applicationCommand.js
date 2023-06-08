7c4f72474f4fb1f845733163b114cf8815e1f316
iciepex <120244424+ich1fx@users.noreply.github.com>
2023-06-09 05:05:05 +0700

    .

--- src/events/applicationCommand.js
+++ src/events/applicationCommand.js
@@ -3,7 +3,7 @@ import * as commands from '../commands/mod.js';
 
 export const type = InteractionType.ApplicationCommand;
   
-export async function execute({ ctx, interaction, branch }) {
+export async function execute({ request, interaction, branch }) {
   const command = Object.values(commands).find(cmd => cmd.data.name === interaction.data.name && cmd.data.type === interaction.data.type);
-  if (command) return await command.execute({ ctx, interaction, branch });
+  if (command) return await command.execute({ request, interaction, branch });
 };
