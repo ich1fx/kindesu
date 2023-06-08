611cc7058288e4251e9df18e6b6ec6c37324457c
iciepex <120244424+ich1fx@users.noreply.github.com>
2023-06-09 04:26:11 +0700

    trying to remove oak

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import { serve } from "http/server.ts";
 import * as routes from './routes/mod.js';
-h
 
-h
-
+serve((request) => {
+  const branch = request.url.host.includes('--') ? request.url.host.split('--')[1].split('.deno.dev')[0] : "PROD";
+  
+  const route = Object.values(routes).find(ctx => ctx.method === request.method && ctx.path === request.url.pathname);
+  if (route) return await route.execute({ request, branch });
+});
