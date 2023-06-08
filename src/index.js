7f642217779613b15df64f461c3939840af37982
iciepex <120244424+ich1fx@users.noreply.github.com>
2023-06-09 05:15:45 +0700

    fix unexpected reserved word

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { serve } from "http/server.ts";
 import * as routes from './routes/mod.js';
 
-serve((request) => {
+serve(async (request) => {
   const branch = request.url.host.includes('--') ? request.url.host.split('--')[1].split('.deno.dev')[0] : "PROD";
   
   const route = Object.values(routes).find(ctx => ctx.method === request.method && ctx.path === request.url.pathname);
