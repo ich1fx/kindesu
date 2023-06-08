284f9b7a03ef69c9b2774c140ae07bac98cf3c9f
iciepex <120244424+ich1fx@users.noreply.github.com>
2023-06-09 05:05:27 +0700

    .

--- src/events/modalSubmit.js
+++ src/events/modalSubmit.js
@@ -3,7 +3,7 @@ import * as modals from '../modals/mod.js';
 
 export const type = InteractionType.ModalSubmit;
 
-export async function execute({ ctx, interaction, branch }) {
+export async function execute({ request, interaction, branch }) {
   const modal = Object.values(modals).find(modal => modal.custom_id === interaction.data.custom_id);
-  if (modal) return await modal.execute({ ctx, interaction, branch });
+  if (modal) return await modal.execute({ request, interaction, branch });
 };
